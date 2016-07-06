43de0446eab4977b068e35da19e077a54ef45d72
schmid <Rafael Schmid>
2016-07-06 10:57:47 -0300

    configs

--- src/config_environment.sh
+++ src/config_environment.sh
@@ -3,8 +3,8 @@ export CUDA_HOME=/usr/local/cuda-7.5
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64  
 PATH=${CUDA_HOME}/bin:${PATH}
 
-export TIME=1
+export TIME=0
 export BITS=5
-export EXECS=10
+export EXECS=1
 export SEGS=1048576
 #export SEGS=128
