ad1cbc010b4eee84dbb72029ec0f05fcaf19f691
Rafael Schmid <rafaelfschmid@gmail.com>
2016-07-13 12:05:33 -0300

    gendiff

--- src/scripts/gendiffseg.sh
+++ src/scripts/gendiffseg.sh
@@ -5,11 +5,11 @@ while [ $c -le 67108864 ]
 do
 	#echo $c 
 	d=1
-	while [ $d -le  32768 ] 
+	while [[ $d -lt $c && $d -le 1048576 ]] 
 	do
 		./diff.exe $d $c > $dir/$d"_"$c.in
 		((d=$d*2))
-		sleep 1
+		sleep 1.0
 	done
 	((c=$c*2))
 done
